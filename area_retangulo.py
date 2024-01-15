b58b277f5f86a8a192fc849ef9828ac3fc7f198d
Dauster Souza Pereira <148724441+Dsp-hub@users.noreply.github.com>
2024-01-15 20:28:27 -0300

    Atualizar a pasta Python com arquivos

---
+++ area_retangulo.py
@@ -0,0 +1,7 @@
+# Programa para calcular a área do retângulo
+# Variáveis utilizadas - area, base, altura, real
+
+base = int(input("Digite o valor da base: "))
+altura = int(input("Digite o valor da altura: ")) 
+area = base * altura
+print("A área do retângulo é", area)
