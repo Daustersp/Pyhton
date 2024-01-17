4abfc88de83300f23ab9bcc2d59a3e076a772367
Dauster Souza Pereira <148724441+Daustersp@users.noreply.github.com>
2024-01-17 19:22:17 -0300

    Create exerc_001.py

---
+++ Semana 1/exerc_001.py
@@ -0,0 +1,4 @@
+aluna = "Julia" #variável aluna recebe uma cadeia de caracteres (string)
+periodo_corrente = 4 #variavel recebe valor inteiro (int)
+ida = 9.99 #variavel índice de rendimento acadêmico (IRA) recebe valor real (float)
+laurea = True #variável recebe valor lógico (boolean)
