9e41710bab085bdfcf683cf8b04f1b3f72cfcdc9
Dauster Souza Pereira <148724441+Daustersp@users.noreply.github.com>
2024-02-27 16:58:35 -0300

    27/02/2024 - If

--- Semana_2/2_6_1_Lista_Exerc_Prof.py
+++ Semana_2/2_6_1_Lista_Exerc_Prof.py
@@ -1,5 +1,7 @@
 num = int(input("Digite um número:"))
 if (num>10):
     print ("É maior que 10")
+elif (num == 0):
+    print("Valor neutro")
 else:
     print ("É menor que 10")
---
+++ Semana_2/aula_teste.py
@@ -0,0 +1,11 @@
+devo_levantar = False
+esta_muito_frio = True
+esta_muito_quente = True
+if devo_levantar == True:
+    print("Levantar e preparar meu lanche")
+elif esta_muito_frio == False:
+    print("Ficar na cama bem quentinha!")
+elif esta_muito_quente == True:
+    print("Diminuir a temperatura do ar condicionado!")
+else:
+    print("Continuar no sono")
