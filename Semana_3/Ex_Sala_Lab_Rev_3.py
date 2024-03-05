53ef5c144222d750fae767c875cb83e0f34dcb8b
Dauster Souza Pereira <148724441+Daustersp@users.noreply.github.com>
2024-03-05 09:22:16 -0300

    Update Ex_Sala_Lab_Rev_3.py

--- Semana_3/Ex_Sala_Lab_Rev_3.py
+++ Semana_3/Ex_Sala_Lab_Rev_3.py
@@ -3,3 +3,17 @@
 
 # qtde_vendida = 1000
 # print("A quantidade vendida foi "+ qtde_vendida)
+import os
+os.system("cls")
+
+# if "dauster" in "dauster.pereira@ifb.edu.br":
+#     print("TEM @")
+# else:
+#     print("NÃO TEM @")
+
+
+if not "ifb" in "dauster.pereira@ifb.edu.br":
+    print("NÃO TEM #")
+else:
+    print("TEM #")
+
